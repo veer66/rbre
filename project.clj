4f3e5fd60476d48dd77bb62925c619bb336ba657
vee <veer66@live.com>
2017-06-12 23:20:00 +0700

    update license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject rbre "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The 2-Clause BSD License"
+            :url "https://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jruby.joni/joni "2.1.10"]]
   :main ^:skip-aot rbre.core
