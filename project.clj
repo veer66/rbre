79a54ea21459a19cc5554f8a7556c95d2a1171c0
Vee Satayamas <vsatayamas@gmail.com>
2018-06-21 21:26:34 +0700

    Relicense and bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject rbre "0.1.1"
+(defproject rbre "0.2.0"
   :description "JRuby's Joni wrapper for Clojure"
   :url "https://gitlab.com/veer66/rbre"
-  :license {:name "The 2-Clause BSD License"
-            :url "https://opensource.org/licenses/BSD-2-Clause"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jruby.joni/joni "2.1.10"]]
   :main ^:skip-aot rbre.core
